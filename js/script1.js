320a92d50f10d4a50e92f9cd9854779582a57ae7
Shawn Bigbie <boboargos@outlook.com>
2019-08-27 09:38:57 -0600

    Update

--- js/script1.js
+++ js/script1.js
@@ -1,13 +1,13 @@
 //document.getElementById('company').innerHTML = "Joe's Bed and Breakfast"
 
-document.querySelector('#company').innerText = "Joe's Bed and Breakfast"
+document.querySelector('#company').innerText = "Shawn's Bed and Breakfast"
 
-document.querySelector('header > h2').innerText = "Best B&B this side of Vernal!"
+document.querySelector('header > h2').innerText = "Best Legacy B&B this side of Utah!"
 
 let userName = prompt("What is your name?")
 
-//let message = "Hello " + userName + ", welcome to the best lodging in Utah!"
+//let message = "Hello " + userName + ", welcome to the best B&B in Utah!"
 
-let message = `Hello ${userName}, welcome to the finest lodging in Utah County`
+let message = `Hello ${userName}, welcome to the finest B&B in Utah, Salt Lake City`
 
 document.querySelector(`#greeting`).innerText = message
