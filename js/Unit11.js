2e470d993a1e3a54ac5aaf8c44781ffa8cf9d3a9
Shawn M Bigbie <boboargos@outlook.com>
2019-11-14 01:14:42 -0700

    Finish Unit 11

---
+++ js/Unit11.js
@@ -0,0 +1,42 @@
+// Document SetUp DOM
+document.querySelector('#header').innerText = "Hotel Booking"
+
+document.querySelector('#topic').innerText = "Get Away from your Kids "
+
+async function getHotelData() {
+    try {
+        const response = await fetch('../hotel.json')
+        return await response.json() // Return the JSON object
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+let hotelData = {}
+getHotelData().then(data => hotelData = data)
+
+document.querySelector("#marriott").addEventListener('click', hotelInfo)
+document.querySelector("#sharaton").addEventListener('click', hotelInfo)
+document.querySelector("#hilton").addEventListener('click', hotelInfo)
+
+function hotelInfo(event) {
+    let hotelChoice = hotelData.hotels.find(hotel => {
+        return event.target.id === hotel.name.toLowerCase()
+    })
+    console.log(hotelChoice)
+
+    document.querySelector("#hotelName").textContent = `${hotelChoice.name} Hotel`
+
+    document.querySelector("#address").textContent = `${hotelChoice.address}`
+
+    document.querySelector("#rooms").textContent = `${hotelChoice.rooms}`
+
+    document.querySelector("#gym").textContent = `${hotelChoice.gym}`
+
+    document.querySelector("#type").textContent = `${hotelChoice.roomTypes}`
+
+    let imagePath = `${hotelChoice.picture}`
+
+    document.querySelector("#picture").setAttribute('src', imagePath)
+
+}
